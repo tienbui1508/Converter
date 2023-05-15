aa97b4720d4e0c1486bf751ecb0183502aeb44c7
tienbui <tienbui1508@gmail.com>
2023-05-15 17:26:19 +0930

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Tien Bui on 15/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
